4986f19452a3ae50c701ce0b69fa64142e4223d7
André Sotero <andre.sotero@telefonica.com>
2021-05-12 09:33:25 -0300

    Criação do projeto

---
+++ src/main/java/com/sotero/estudoifelse/Main.java
@@ -0,0 +1,16 @@
+package com.sotero.estudoifelse;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(":::...ESTUDO IF & ELSE...:::");
+        
+        
+        /*
+            COLOCAR SEU CÓDIGO AKI
+        */
+        
+        
+        System.out.println(":::...FIM ESTUDO IF & ELSE...:::");
+    }
+}
